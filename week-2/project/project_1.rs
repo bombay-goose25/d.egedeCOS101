adb6c9cf6bd312fb64a9e1021c42e73c742a10f1
bombbay-goose25 <darryl.egede@pau.edu.ng>
2025-10-09 18:27:52 +0100

    (my second commit)

--- week-2/project/project_1.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-	let p = 520000000.0;
-	let r = 10.0;
-	let n = 5.0;
-	let a = p*(1.0 + r/100.0).powf(n);
-	let ci = a - p;
-	println!("Compound Interest = {}",ci);
-	println!("Total Amount = {}",a);
-}
--- week-2/project/project_3.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-	let p = 510000.0;
-	let r = 5.0;
-	let n = 3.0;
-	let a = p*(1.0 - r/100.0).powi(n);
-	println!("Value after depreciation = {}",a);
-}
