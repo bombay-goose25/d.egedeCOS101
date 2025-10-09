595c95ca0a2228faa0742898e6e88b95c5d4b657
bombbay-goose25 <darryl.egede@pau.edu.ng>
2025-10-09 18:22:37 +0100

    (my second commit)

--- week-2/project/project_3.rs
+++ week-2/project/project_3.rs
@@ -2,6 +2,6 @@ fn main() {
 	let p = 510000.0;
 	let r = 5.0;
 	let n = 3.0;
-	let a = p*(1.0 - r/100.0).powf(n);
+	let a = p*(1.0 - r/100.0).powi(n);
 	println!("Value after depreciation = {}",a);
 }
