3effaabc18d66a754377a685faaced54dc8be39e
bombay-goose25 <darryl.egede@pau.edu.ng>
2025-11-26 13:40:24 +0100

    appending data to a file

--- week-9/practice_4/src/main.rs
+++ week-9/practice_4/src/main.rs
@@ -4,6 +4,6 @@ use::std::io::Write;
 fn main() {
     let mut file = OpenOptions::new().append(true).open("data.txt").expect("cannot open file");
     file.write_all("\nhello class".as_bytes()).expect("write failed");
-    file.write_all("\nthis is the appendage to the document".as_bytes()).expect("write failed");
+    file.write_all("\nThis is the appendage to the document".as_bytes()).expect("write failed");
     println!("file append success");
 }
