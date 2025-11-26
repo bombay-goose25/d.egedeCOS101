e8dfb2834f9aa898b3f08e17f0d868feeb71c51a
bombay-goose25 <darryl.egede@pau.edu.ng>
2025-11-26 13:38:42 +0100

    reading a file

--- week-9/practice_3/src/main.rs
+++ week-9/practice_3/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::Read;
 fn main() {
     let mut file = std::fs::File::open("welcome_message.txt").unwrap();
-    let mut content = String::new();
-    file.read_to_string(&mut content).unwrap();
-    println!("{}",content);
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("{}",contents);
 }
