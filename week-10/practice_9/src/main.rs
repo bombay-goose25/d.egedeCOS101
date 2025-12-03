baf77dc7c5e14b14f02ac821c8500b1f56964f77
bombbay-goose25 <darryl.egede@pau.edu.ng>
2025-12-03 13:36:58 +0100

    ownership

---
+++ week-10/practice_9/src/main.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct Rectangle {
+    width:u32, height:u32
+}
+impl Rectangle {
+    fn area (&self)->u32{
+        self.width * self.height
+    }
+}
+fn main() {
+    let small = Rectangle{
+        width:10,
+        height:20
+    };
+    println!("width is {} \n height is {} \n area of rectangle {}",small.width,small.height,small.area());
+}
